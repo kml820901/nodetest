683dfe3876141c4cf0da1428007b611f12ca8a46
mlkang <mlkang@trinitesolutions.com>
2018-04-24 00:09:21 +0800

    oooo

--- NodeTest/hello-world-server.js
+++ NodeTest/hello-world-server.js
@@ -1,5 +1,6 @@
 //test222
 var http = require('http');
+var aaa = "";
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
